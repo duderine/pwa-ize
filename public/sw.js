76a0694765566e7f58d65c57bb4eb50eb237d964
duderine <110584103+duderine@users.noreply.github.com>
2026-03-13 14:37:04 -0300

    Delete public/sw.js

--- public/sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', (e) => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(clients.claim()));
-self.addEventListener('fetch', (e) => {
-    // Permite que o redirecionamento aconteça sem interceptação
-    e.respondWith(fetch(e.request));
-});
